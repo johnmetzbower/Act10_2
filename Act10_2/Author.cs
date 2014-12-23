e5c4d1cca16fcd7607d2d8e737b6aec5cecfa260
johnmetzbower <johnmetzbower@nokware.net>
2014-12-22 20:59:57 -0600

    Act10_2

---
+++ Act10_2/Author.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient; 
+
+
+namespace Act10_2
+{
+    public class Author
+    {
+        SqlConnection _pubConnection;
+        string _connString;
+        SqlDataAdapter _pubDataAdapter;
+        DataSet authorDataSet;
+        public Author()
+        {
+            _connString =
+                @"Data Source=(localdb)\ProjectsV12;Initial Catalog=Pubs;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False";
+            _pubConnection = new SqlConnection();
+            _pubConnection.ConnectionString = _connString;
+            SqlCommand selectCommand =
+                new SqlCommand("Select au_id, au_lname,au_fname from authors", _pubConnection);
+            _pubDataAdapter = new SqlDataAdapter();
+            _pubDataAdapter.SelectCommand = selectCommand;
+        }
+        public DataSet GetData()
+        {
+            try
+            {
+                authorDataSet = new DataSet();
+                _pubDataAdapter.Fill(authorDataSet, "Author");
+                return authorDataSet;
+            }
+            catch (Exception ex)
+            { 
+                throw ex;
+            }
+        }
+    }
+ }
+
